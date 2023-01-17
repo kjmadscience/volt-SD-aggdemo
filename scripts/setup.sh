70a9a015a977dddd4be435931de678aaedc29438
David Rolfe <drolfe@voltdb.com>
2023-01-17 10:22:58 +0000

    Added aggdemo.json

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,4 +7,7 @@ cd voltdb-aggdemo/scripts
 
 sleep 120
 sqlcmd --servers=`cat $HOME/.vdbhostnames` < ../ddl/voltdb-aggdemo-createDB.sql
+
+$HOME/bin/reload_dashboards.sh aggdemo.json
+
 java -jar $HOME/bin/addtodeploymentdotxml.jar `cat $HOME/.vdbhostnames` deployment topics.xml
